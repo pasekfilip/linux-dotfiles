531dd8bd4b017d0a261b258b94f979eeca94c16b
filip psek <filip.pasek6@gmail.com>
2025-11-11 12:20:45 +0100

    Add image preview package as part of oil conf

--- nvim/.config/nvim/lua/filip/plugins/oil.lua
+++ nvim/.config/nvim/lua/filip/plugins/oil.lua
@@ -1,16 +1,27 @@
 return {
-	'stevearc/oil.nvim',
-	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-	lazy = false,
-	config = function()
-		require("oil").setup {
-			columns = { "icon" },
-			keymaps = {
-				["<C-s>"] = false,
-				["<C-h>"] = false,
-				["<C-l>"] = false,
-			}
-		}
-	end,
-	vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    {
+        'stevearc/oil.nvim',
+        dependencies = {
+        { 'nvim-tree/nvim-web-devicons' },
+        { 'adelarsq/image_preview.nvim', lazy = true },
+        },
+        lazy = false,
+        config = function()
+            local image_preview = require("image_preview");
+            require("oil").setup {
+                columns = { "icon" },
+                keymaps = {
+                    ["<leader>i"] = {
+                        callback = function()
+                            image_preview.PreviewImageOil();
+                        end,
+                    },
+                    ["<C-s>"] = false,
+                    ["<C-h>"] = false,
+                    ["<C-l>"] = false,
+                }
+            }
+        end,
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    }
 }
