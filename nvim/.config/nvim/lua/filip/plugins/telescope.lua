b0f4c28312db7c263c27d8d7f28c93b244536d51
filip psek <filip.pasek6@gmail.com>
2026-02-09 11:50:18 +0100

    Add keymap for telescope diagnostics

--- nvim/.config/nvim/lua/filip/plugins/telescope.lua
+++ nvim/.config/nvim/lua/filip/plugins/telescope.lua
@@ -1,53 +1,59 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        "nvim-tree/nvim-web-devicons",
-        "folke/todo-comments.nvim",
-    },
-    config = function()
-        local telescope = require("telescope")
-        local actions = require("telescope.actions")
-        local builtin = require("telescope.builtin")
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+		"folke/todo-comments.nvim",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local builtin = require("telescope.builtin")
 
-        telescope.setup({
-            defaults = {
-                path_display = { "smart" },
-                mappings = {
-                    i = {
-                        ["<C-h>"] = function(prompt_bufnr)
-                            local prompt = require("telescope.actions.state").get_current_line()
-                            actions.close(prompt_bufnr);
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-h>"] = function(prompt_bufnr)
+							local prompt = require("telescope.actions.state").get_current_line()
+							actions.close(prompt_bufnr)
 
-                            builtin.find_files({
-                                hidden = true,
-                                default_text = prompt,
-                            })
-                        end,
-                        ["<C-s>"] = actions.select_vertical,
-                    },
-                },
-            },
-            pickers = {
-                -- find_files = {
-                --     theme = "dropdown",
-                -- }
-            },
-        })
+							builtin.find_files({
+								hidden = true,
+								default_text = prompt,
+							})
+						end,
+						["<C-s>"] = actions.select_vertical,
+					},
+				},
+			},
+			pickers = {
+				-- find_files = {
+				--     theme = "dropdown",
+				-- }
+			},
+		})
 
-        telescope.load_extension("fzf")
+		telescope.load_extension("fzf")
 
-        -- set keymaps
-        local keymap = vim.keymap
+		-- set keymaps
+		local keymap = vim.keymap
 
-        keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
-        keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
-        keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in cwd" })
-        keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
-        keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help tags" })
-        keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    end,
+		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Get diagnostics" })
+
+		keymap.set("n", "<leader>fe", function()
+			builtin.diagnostics({ severity = "ERROR" })
+		end, { desc = "Workspace Errors" })
+
+		keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help tags" })
+		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+	end,
 }
