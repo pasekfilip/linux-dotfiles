86b0ab78e4854d6bca736ff7a33d85aa232463cc
filip psek <filip.pasek6@gmail.com>
2026-04-15 10:11:12 +0200

    Add default code action keymap

--- nvim/.config/nvim/lua/filip/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/filip/plugins/lsp/lspconfig.lua
@@ -35,8 +35,8 @@ return {
 				opts.desc = "Show LSP type definitions"
 				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-				-- opts.desc = "See available code actions"
-				-- keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+				opts.desc = "See available code actions"
+				keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
 				opts.desc = "Smart rename"
 				keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts) -- smart rename
