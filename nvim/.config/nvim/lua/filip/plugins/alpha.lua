ea37b89483bcee5be24d032c24ded2ebd15c5a8d
filip psek <filip.pasek6@gmail.com>
2026-04-16 10:37:32 +0200

    Alpha afffff

--- nvim/.config/nvim/lua/filip/plugins/alpha.lua
+++ nvim/.config/nvim/lua/filip/plugins/alpha.lua
@@ -1,12 +1,7 @@
 return {
 	"goolord/alpha-nvim",
-	-- dependencies = { 'nvim-mini/mini.icons' },
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		local startify = require("alpha.themes.startify")
-		-- available: devicons, mini, default is mini
-		-- if provider not loaded and enabled is true, it will try to use another provider
-		startify.file_icons.provider = "devicons"
-		require("alpha").setup(startify.config)
+		require("alpha").setup(require("alpha.themes.dashboard").config)
 	end,
 }
