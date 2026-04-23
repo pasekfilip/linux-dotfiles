28e8f4d0f0bd786f83a26421ffb75ac9cf3a0977
filip psek <filip.pasek6@gmail.com>
2026-04-23 12:54:06 +0200

    Update alfik

--- nvim/.config/nvim/lua/filip/plugins/alpha.lua
+++ nvim/.config/nvim/lua/filip/plugins/alpha.lua
@@ -1,7 +1,34 @@
 return {
 	"goolord/alpha-nvim",
-	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VimEnter",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("alpha").setup(require("alpha.themes.dashboard").config)
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", "<cmd>ene <BAR> startinsert <cr>"),
+			dashboard.button("s", "󱑒  Restore Session", [[<cmd>lua require("persistence").load()<cr>]]),
+			dashboard.button("d", "  Database UI (Dadbod)", "<cmd>DBUIToggle<cr>"),
+			dashboard.button("p", "  Find Project", "<cmd>Telescope find_files<cr>"),
+			dashboard.button("q", "󰅚  Quit", "<cmd>qa<cr>"),
+		}
+
+		local stats = require("lazy").stats()
+		dashboard.section.footer.val = "⚡ Loaded " .. stats.count .. " plugins"
+
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
 	end,
 }
