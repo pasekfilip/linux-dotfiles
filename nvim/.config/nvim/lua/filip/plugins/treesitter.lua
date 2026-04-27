dd5c58e8d845b36e08dc85fda128be197fcb1a80
filip psek <filip.pasek6@gmail.com>
2026-04-27 12:45:08 +0200

    Migrate treesitter conf to nvim 0.12

--- nvim/.config/nvim/lua/filip/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/filip/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
 	lazy = false,
 	build = ":TSUpdate",
 
 	dependencies = {
-		{ "nvim-treesitter/nvim-treesitter-textobjects" },
+		{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
 		"Badhi/nvim-treesitter-cpp-tools",
 		-- {
 		-- 	"nvim-treesitter/nvim-treesitter-context",
@@ -19,59 +20,84 @@ return {
 		-- },
 	},
 
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+	init = function()
+		-- Enable treesitter highlighting and indentation via FileType autocmd
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				-- Enable treesitter highlighting and disable regex syntax
+				pcall(vim.treesitter.start)
+				-- Enable treesitter-based indentation
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
 
-		treesitter.setup({
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-			ensure_installed = {
-				"gdscript",
-				"godot_resource",
-				"json",
-				"javascript",
-				"typescript",
-				"yaml",
-				"html",
-				"css",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"vimdoc",
-				"c_sharp",
-				"cpp",
-				"python",
-				"java",
-			},
-			auto_install = true,
-			sync_install = false,
-			ignore_install = {},
-			modules = {},
-			textobjects = {
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["[c"] = "@class.outer",
-					},
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]c"] = "@class.outer",
-					},
-				},
+		-- Install parsers that aren't already installed
+		local ensureInstalled = {
+			"gdscript",
+			"godot_resource",
+			"json",
+			"javascript",
+			"typescript",
+			"yaml",
+			"html",
+			"css",
+			"markdown",
+			"markdown_inline",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"vimdoc",
+			"c_sharp",
+			"cpp",
+			"python",
+			"java",
+		}
+
+		local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+		local parsersToInstall = vim.iter(ensureInstalled)
+			:filter(function(parser)
+				return not vim.tbl_contains(alreadyInstalled, parser)
+			end)
+			:totable()
+
+		if #parsersToInstall > 0 then
+			require("nvim-treesitter").install(parsersToInstall)
+		end
+	end,
+
+	config = function()
+		-- Configure textobjects (new API for main branch)
+		require("nvim-treesitter-textobjects").setup({
+			move = {
+				set_jumps = true, -- whether to set jumps in the jumplist
 			},
 		})
 
+		-- Set up textobject movement keymaps
+		local ts_move = require("nvim-treesitter-textobjects.move")
+
+		-- Jump to next function/class start
+		vim.keymap.set({ "n", "x", "o" }, "]m", function()
+			ts_move.goto_next_start("@function.outer", "textobjects")
+		end, { desc = "Next function start" })
+
+		vim.keymap.set({ "n", "x", "o" }, "]c", function()
+			ts_move.goto_next_start("@class.outer", "textobjects")
+		end, { desc = "Next class start" })
+
+		-- Jump to previous function/class start
+		vim.keymap.set({ "n", "x", "o" }, "[m", function()
+			ts_move.goto_previous_start("@function.outer", "textobjects")
+		end, { desc = "Previous function start" })
+
+		vim.keymap.set({ "n", "x", "o" }, "[c", function()
+			ts_move.goto_previous_start("@class.outer", "textobjects")
+		end, { desc = "Previous class start" })
+
+		-- Configure C++ tools
 		require("nt-cpp-tools").setup({
 			preview = {
 				quit = "q", -- quit preview window
@@ -86,6 +112,7 @@ return {
 			},
 		})
 
+		-- Set up keymaps for C++ tools
 		vim.keymap.set("n", "<leader>cf", "<cmd>TSCppDefineClassFunc<cr>", { desc = "Draft Class Functions" })
 		vim.keymap.set("v", "<leader>cf", ":TSCppDefineClassFunc<cr>", { desc = "Draft Selected Functions" })
 		vim.keymap.set("n", "<leader>cm", "<cmd>TSCppMakeConcreteClass<cr>", { desc = "Make Concrete Class" })
