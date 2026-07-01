4c8387f70b1ab2b671f68010d3e9cd4f5bc30129
filip psek <filip.pasek6@gmail.com>
2026-07-01 11:19:29 +0200

    Make kulala binding scoped to .http and .rest

--- nvim/.config/nvim/lua/filip/plugins/kulala.lua
+++ nvim/.config/nvim/lua/filip/plugins/kulala.lua
@@ -2,7 +2,49 @@ return {
 	"mistweaverco/kulala.nvim",
 	ft = { "http", "rest" },
 	opts = {
-		global_keymaps = true,
+		-- Scope every kulala keymap to http/rest buffers so the <leader>i prefix
+		-- doesn't clash with the global <leader>i task-popup toggle. By default
+		-- these 5 have no `ft` and bind globally, turning <leader>i into an
+		-- ambiguous prefix everywhere (task toggle then waits for timeoutlen).
+		global_keymaps = {
+			["Open scratchpad"] = {
+				"<leader>ib",
+				function()
+					require("kulala").scratchpad()
+				end,
+				ft = { "http", "rest" },
+			},
+			["Open kulala"] = {
+				"<leader>io",
+				function()
+					require("kulala").open()
+				end,
+				ft = { "http", "rest" },
+			},
+			["Send request"] = {
+				"<leader>is",
+				function()
+					require("kulala").run()
+				end,
+				mode = { "n", "v" },
+				ft = { "http", "rest" },
+			},
+			["Send all requests"] = {
+				"<leader>ia",
+				function()
+					require("kulala").run_all()
+				end,
+				mode = { "n", "v" },
+				ft = { "http", "rest" },
+			},
+			["Replay the last request"] = {
+				"<leader>ir",
+				function()
+					require("kulala").replay()
+				end,
+				ft = { "http", "rest" },
+			},
+		},
 		global_keymaps_prefix = "<leader>i",
 		kulala_keymaps_prefix = "",
 	},
