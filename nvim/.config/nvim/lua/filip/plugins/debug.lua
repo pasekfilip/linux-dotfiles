5e66fbec51df155a14a9fa38e1f9a640db96d9e8
filip psek <filip.pasek6@gmail.com>
2026-08-09 13:02:47 +0200

    Update debug conf nvim

--- nvim/.config/nvim/lua/filip/plugins/debug.lua
+++ nvim/.config/nvim/lua/filip/plugins/debug.lua
@@ -1,26 +1,56 @@
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"theHamsta/nvim-dap-virtual-text",
+		"igorlfs/nvim-dap-view",
 		"williamboman/mason.nvim",
+	},
+	keys = {
+		{
+			"<leader>dc",
+			function() require("dap").continue() end,
+			desc = "Debug: Start/Continue",
+		},
+		{
+			"<leader>dq",
+			function() require("dap").terminate() end,
+			desc = "Debug: Terminate",
+		},
+		{
+			"<leader>dt",
+			function() require("dap-view").toggle() end,
+			desc = "Debug: Toggle View",
+		},
+		{
+			"<leader>db",
+			function() require("dap").toggle_breakpoint() end,
+			desc = "Debug: Toggle Breakpoint",
+		},
+		{
+			"<leader>do",
+			function() require("dap").step_over() end,
+			desc = "Debug: Step Over",
+		},
+		{
+			"<leader>di",
+			function() require("dap").step_into() end,
+			desc = "Debug: Step Into",
+		},
+		{
+			"<leader>dO",
+			function() require("dap").step_out() end,
+			desc = "Debug: Step Out",
+		},
 	},
 	config = function()
 		local dap = require("dap")
-		local dapui = require("dapui")
-		local virtual_text = require("nvim-dap-virtual-text")
+		local dapview = require("dap-view")
 
-		dapui.setup()
-		virtual_text.setup({
-			enabled = true, -- enable this plugin (default)
-			enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, etc.
-			highlight_changed_variables = true, -- highlight changed values with a different color
-			highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables
-			show_stop_reason = true, -- show stop reason when stopped on a breakpoint
-			commented = true, -- prefix virtual text with comment string
-			only_manually_hidden = false, -- only hide virtual text when issued a command
-			-- virt_text_pos = 'eol',              -- position of virtual text, see `:h nvim_buf_set_extmark()`
+		dapview.setup({
+			auto_toggle = true, -- open on launch/attach, close when the session ends
+			virtual_text = {
+				enabled = true,
+				position = "eol",
+			},
 		})
 
 		dap.adapters.java = function(callback)
@@ -50,16 +80,6 @@ return {
 				args = { "--port", "${port}" },
 			},
 		}
-
-		dap.listeners.after.event_initialized["dapui_config"] = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated["dapui_config"] = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited["dapui_config"] = function()
-			dapui.close()
-		end
 
 		dap.configurations.cpp = {
 			{
@@ -102,16 +122,5 @@ return {
 				stopOnEntry = false,
 			},
 		}
-
-		local set = vim.keymap.set
-		set("n", "<F4>", dap.continue, { desc = "Debug: Start/Continue" })
-		set("n", "<F7>", function()
-			dap.terminate()
-			dapui.close()
-		end, { desc = "Debug: Terminate" })
-		set("n", "<F8>", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-		set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
-		set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
-		set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
 	end,
 }
