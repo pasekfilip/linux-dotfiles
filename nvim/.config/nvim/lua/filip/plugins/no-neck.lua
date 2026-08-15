23b81b01fc3f0f9f339af42ada5d9796295d2071
pasekfilip <filip.pasek6@gmail.com>
2026-08-15 13:14:15 +0200

    Nvim no neck pain plugin

---
+++ nvim/.config/nvim/lua/filip/plugins/no-neck.lua
@@ -0,0 +1,8 @@
+return {
+	"shortcuts/no-neck-pain.nvim",
+    keys = {
+        {"<leader>np","<cmd>NoNeckPain<cr>"},
+        {"<leader>nl","<cmd>NoNeckPainToggleRightSide<cr>"},
+        {"<leader>nh","<cmd>NoNeckPainToggleLeftSide<cr>"}
+    }
+}
