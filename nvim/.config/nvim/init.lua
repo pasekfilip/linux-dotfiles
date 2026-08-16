4ff027044e2d06d306b98ac69e734dfa84536a44
pasekfilip <filip.pasek6@gmail.com>
2026-08-16 17:52:37 +0200

    Omarchy theme bridge

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,5 @@
 require("filip.core")
 require("filip.lazy")
+-- Needs lazy.setup() to have run: it loads the colorscheme plugin for the
+-- active Omarchy theme on demand.
+require("filip.omarchy").setup()
